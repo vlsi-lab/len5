a4373123ab199615f744427c14065808cfb87095
Michele Caon <michele.caon@polito.it>
2022-10-05 10:03:57 +0200

    Update hello test

--- sw/test-programs/src/hello.c
+++ sw/test-programs/src/hello.c
@@ -2,6 +2,14 @@
 
 int main(void)
 {
-    printf("Hello LEN%d!", 5);
+    int a = 0;
+
+    for (size_t i = 0; i < 10; i++)
+    {
+        a++;
+    }
+
+    printf("Hello LEN%d!\n", a >> 1);
+    // puts("Hello, LEN5!");
     return 0;
 }
