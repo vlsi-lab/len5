c71cab329c5be4f8b591c1af2b937a897ef52945
Vincenzo-Petrolo <s295043@studenti.polito.it>
2024-02-21 16:59:08 +0100

    chore : added patch in vendor script

--- sw/benchmarks/embench/support/main.c
+++ sw/benchmarks/embench/support/main.c
@@ -12,6 +12,7 @@
    SPDX-License-Identifier: GPL-3.0-or-later */
 
 #include "support.h"
+#include <stdio.h>
 
 
 int __attribute__ ((used))
@@ -21,18 +22,24 @@ main (int argc __attribute__ ((unused)),
   int i;
   volatile int result;
   int correct;
+  int mcycle_start, mcycle_stop;
+  int minstret_start, minstret_stop;
 
   initialise_benchmark ();
   warm_caches (WARMUP_HEAT);
 
-  start_trigger ();
+  asm volatile ("csrr %0, mcycle" : "=r" (mcycle_start));
+  asm volatile ("csrr %0, minstret" : "=r" (minstret_start));
   result = benchmark ();
-  stop_trigger ();
+  asm volatile ("csrr %0, mcycle" : "=r" (mcycle_stop));
+  asm volatile ("csrr %0, minstret" : "=r" (minstret_stop));
+
 
   /* bmarks that use arrays will check a global array rather than int result */
-
   correct = verify_benchmark (result);
 
+  printf("Cycles: %d\n", mcycle_stop - mcycle_start);
+  printf("Instructions: %d\n", minstret_stop - minstret_start);
   return (!correct);
 
 }				/* main () */
