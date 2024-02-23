bdb596a5e1d75330163e46c0c1f6d64801196f0d
Vincenzo-Petrolo <s295043@studenti.polito.it>
2024-02-23 17:38:49 +0100

    chore : set constraint, closes Issue #5

--- syn/set-constraints.tcl
+++ syn/set-constraints.tcl
@@ -1,11 +1,12 @@
 # Clock period (in ns)
-set CLK_PERIOD 15
+set CLK_PERIOD 2
 
 # Inputs delay
-set INPUT_DELAY 0.5
+# divide clk period by two
+set INPUT_DELAY [expr $CLK_PERIOD / 2]
 
-# Output delay (in ns)
-set OUTPUT_DELAY 0.5
+# Output delay
+set OUTPUT_DELAY [expr $CLK_PERIOD / 2]
 
 # Output load
 set OLOAD [load_of tcbn65lpwc/BUFFD24/I]
