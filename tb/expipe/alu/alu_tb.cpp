62e9d4f51e2dc630f691c373df5ad89725db8382
Mattia Mirigaldi <s288224@studenti.polito.it>
2024-02-27 11:39:46 +0100

    Added corner case to alu tb

--- tb/expipe/alu/alu_tb.cpp
+++ tb/expipe/alu/alu_tb.cpp
@@ -24,6 +24,8 @@
 #define MAX_SIM_TIME 1e7
 #define FST_FILENAME "logs/waves.fst"
 
+#define array_size(x) (sizeof(x) / sizeof(x[0]))
+
 // Testbench logger
 TbLogger logger;
 
@@ -192,6 +194,27 @@ void rstDut(Valu *dut, vluint64_t sim_time)
     }
 }
 
+// Corner cases for the input transaction
+const vluint64_t corner_cases[] = {
+    0x0000000000000000, // zero
+    0x0000000000000001, // 64 bits min_positive
+    0x7FFFFFFFFFFFFFFF, // 64 bits max_positive
+    0x8000000000000000, // 64 bits min_negative
+    0xFFFFFFFFFFFFFFFF, // 64 bits max_negative
+    0x000000007FFFFFFF, // least 32 bits max_positive
+    0x0000000080000000, // least 32 bits min_negative
+    0x00000000FFFFFFFF, // least 32 bits max_negative
+    0x0000000100000000, // most 32 bits min_positive
+    0x7FFFFFFF00000000, // most 32 bits max_positive
+    0xFFFFFFFF00000000, // most 32 bits min_negative
+    0xAAAAAAAAAAAAAAAA, // 64 bits even
+    0x5555555555555555, // 64 bits odd
+    0x00000000AAAAAAAA, // least 32 bits even
+    0x0000000055555555, // least 32 bits odd
+    0xAAAAAAAA00000000, // most 32 bits even
+    0x5555555500000000 // most 32 bits odd
+};
+
 /* Generate an input transaction 
    Return a request object*/
 ReqTx *genReqTx()
@@ -215,8 +238,20 @@ ReqTx *genReqTx()
         req->flush = 1;
         return req;
     }
-
-    // Generate a valid transaction with 75% probability
+    // Corner case with 1% probability
+    if (p == 26)
+    {
+        req->valid = 1;
+        req->flush = 0;
+        req->rs1 = corner_cases[rand() % (array_size(corner_cases)-1)];
+        req->rs2 = corner_cases[rand() % (array_size(corner_cases)-1)];
+        // rob_ix assigned randomly, it does not have an actual effect on ALU 
+        req->rob_idx = rand() % (Valu_len5_config_pkg::ROB_DEPTH - 1);
+        p = rand() % 14;
+        req->alu_ctl= Valu_expipe_pkg::alu_ctl_t(p);
+        return req;
+    }
+    // Generate a valid transaction with 74% probability
     req->valid = 1;
     req->flush = 0;
     req->rs1 = vl_rand64();
@@ -227,3 +262,4 @@ ReqTx *genReqTx()
     req->alu_ctl= Valu_expipe_pkg::alu_ctl_t(p);
     return req;
 }
+
