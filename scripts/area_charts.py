b2bd109125837066e6ddbb78bbb7a99909b160a2
Vincenzo-Petrolo <s295043@studenti.polito.it>
2024-03-01 16:27:24 +0100

    chore : improved area script

--- scripts/area_charts.py
+++ scripts/area_charts.py
@@ -45,6 +45,12 @@ with open(report_file, 'r') as fp:
             
         if (float(parts[2]) < 1): # Skip submodules with less than 1% utilization
             continue
+            
+        # Re-name submodule name to keep last u_... part
+        parts[0] = parts[0].split('u_')[-1]
+
+        parts[0] = parts[0].replace('_rs', '')
+        parts[0] = parts[0].replace('arith', 'alu')
         
         data['Submodule'].append(str(parts[0]))
         data['Area'].append(float(parts[1]))
@@ -58,28 +64,22 @@ print(df)
 colors = ["#6d1a3680", "#6d1a36c0", "#6d1a36ff", "#00748080", "#007480c0", "#007480ff"]
 df_bars = df[["Submodule", "Utilization Percentage"]]
 
-df_bars.plot(kind="pie", rot=0, y='Utilization Percentage', x='Submodule', title="Area Utilization per Submodule", figsize=(12, 5), color=colors)
+# Sort by utilization percentage and take the top len(colors)
+df_bars = df_bars.sort_values(by="Utilization Percentage", ascending=False).head(len(colors))
+
+ax = df_bars.plot(kind="pie", y='Utilization Percentage', labels=df_bars['Submodule'], title="Area Utilization per Submodule", figsize=(12, 5), colors=colors, legend=False, autopct='%1.1f%%')
+
+ax.set_ylabel("")
 
 # Draw horizontal grid only
 plt.gca().yaxis.grid(True)
 
 # Set suptitle
-plt.suptitle("LEN5 Processor IPC Improvement", fontweight="bold", fontsize="x-large", color="#3d3d3dff")
-
-# Superpose an horizontal line at y=1 with label "CPU"
-# plt.axhline(y=1, color="#3d3d3dff", linestyle="--")
-# plt.text(-0.62, 2, "CPU", color="#3d3d3dff", rotation=0, fontsize="small")
-
-# Set Y max value
-# plt.gca().set_ylim([0, 1]) //TODO uncomment this if just IPC
-
-# Add 'x' to the y-axis step labels
-# plt.gca().yaxis.set_major_formatter(lambda x, pos: str(int(x)) + "x")
+plt.suptitle("LEN5 Area Partitions", fontweight="bold", fontsize="x-large", color="#3d3d3dff")
 
 # Set legend titles
 plt.gca().set_title(plt.gca().get_title(), fontsize="large", color="#3d3d3dff")
-plt.gca().legend(title="Processor")
-# plt.gca().set_xlabel(plt.gca().get_xlabel(), fontweight="bold")
-# plt.gca().set_ylabel(plt.gca().get_ylabel(), fontweight="bold")
+plt.gca().set_xlabel(plt.gca().get_xlabel(), fontweight="bold")
+plt.gca().set_ylabel(plt.gca().get_ylabel(), fontweight="bold")
 
 plt.savefig(chart_file, dpi=600, bbox_inches="tight")
